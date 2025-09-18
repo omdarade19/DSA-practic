25a8e379806af0e2e9810e6287d34b08f58731f2
omdarade19 <omdarade1944@gmail.com>
2025-09-18 19:28:33 +0530

    initial commit

---
+++ findEven.java
@@ -0,0 +1,13 @@
+public class findEven {
+
+    public static void main(String[] args) {
+        int n= 4;
+
+        if(n%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+
+    }
+}
