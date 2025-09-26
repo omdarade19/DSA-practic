1bc258968e0e1965021cc3f886acb91eac9e1857
omdarade19 <omdarade1944@gmail.com>
2025-09-26 19:58:43 +0530

    max permutations

---
+++ String/maxPermutations.java
@@ -0,0 +1,35 @@
+package String;
+
+public class maxPermutations {
+
+    public static long factorial(int N){
+        long fact = 1;
+        for(int i=2; i<=N;i++){
+            fact *=i;
+        }
+        return fact;
+    }
+
+    public static long maxPermutations(String [] arr, int N){
+        int maxLength=0;
+        for(String s : arr){
+            String noVovels = s.replaceAll("(?i)[aeiou]","")
+//            System.out.println(noVovels);
+            maxLength= Math.max(maxLength,noVovels.length());
+        }
+
+        if(maxLength ==0){
+            return 0;
+        }
+//        System.out.println(maxLength);
+
+        return factorial(maxLength);
+    }
+
+    public static void main(String[] args){
+        String [] arr={"eio"};
+        int N = 1;
+
+        System.out.println("the maxpermutations is :"+maxPermutations(arr,N));
+    }
+}
