9eb532057fe3edf930d68de7cd05d334b2be6e6f
omdarade19 <omdarade1944@gmail.com>
2025-09-28 16:30:02 +0530

    HashMap Example for Iterations and removal

--- Hashing/ExamplesHashMaps.java
+++ Hashing/ExamplesHashMaps.java
@@ -1,6 +1,8 @@
 package Hashing;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class ExamplesHashMaps {
 
@@ -16,14 +18,37 @@ public class ExamplesHashMaps {
 
         map.put("China", 170);
 
-//        if(map.containsKey("China")){
-//            System.out.println("is is Contain the key");
-//        }else{
-//            System.out.println("Not contains key");
-//        }
+        if(map.containsKey("China")){
+            System.out.println("is is Contain the key");
+        }else{
+            System.out.println("Not contains key");
+        }
 
         System.out.println(map.get("China"));
         System.out.println(map.get("Indo"));
+
+        // Iterations on the map
+
+       for( Map.Entry<String,Integer>e:map.entrySet()){
+           System.out.println(e.getKey());
+           System.out.println(e.getValue());
+       }
+
+       //2nd option for iterations
+        Set<String> keys = map.keySet();
+
+       for(String key : keys){
+           System.out.println(key);
+           System.out.println(map.get(key));
+       }
+
+        map.remove("China");
+        map.remove("US");
+        map.put("Rushia", 25);
+        map.put("Bharat",150);
+        map.put("Afganistan",26);
+
+        map.remove("India");
 
         System.out.println(map);
 
