c2fea529e4c53fb7385dbf7f674b6f91f9975468
omdarade19 <omdarade1944@gmail.com>
2025-09-30 19:52:53 +0530

    hashSet examples Syntax

---
+++ Hashing/HashSetExample.java
@@ -0,0 +1,27 @@
+package Hashing;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+
+    public static void main(String[] args){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(1);
+
+        if(set.contains(1)){
+            System.out.println("the set contains 1");
+        }
+
+        set.remove(2);
+
+        System.out.println("the size of the set is :"+ set.size());
+
+        System.out.println(set);
+    }
+}
