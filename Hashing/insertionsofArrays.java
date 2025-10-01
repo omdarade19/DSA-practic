81b2ac699128abf38e09b1b2e434069bf91a1847
omdarade19 <omdarade1944@gmail.com>
2025-10-01 20:12:29 +0530

    the common elements in 2 arrays using HashSet

---
+++ Hashing/insertionsofArrays.java
@@ -0,0 +1,32 @@
+package Hashing;
+import java.util.HashSet;
+public class insertionsofArrays {
+
+    public static int InsertionsofArrays(int[] arr, int[] arr2){
+        int n= arr.length, m=arr2.length;
+        int count =0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<n; i++){
+            set.add(arr[i]);
+        }
+
+        for(int j=0; j<m; j++){
+            if(set.contains(arr2[j])){
+                System.out.println(arr2[j]);
+                count++;
+                set.remove(arr2[j]);
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int arr[] = {7,4,3,2,9}, arr2[] = {6,3,9,7,4,2,9};
+
+        int Ans=InsertionsofArrays(arr, arr2);
+        System.out.println("the common Elements are :"+Ans);
+
+    }
+}
