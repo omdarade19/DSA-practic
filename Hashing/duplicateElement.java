43fcfc565bc77d5c376006ebb76766648207a6ae
omdarade19 <omdarade1944@gmail.com>
2025-10-14 06:23:46 +0530

    duplicate Array

---
+++ Hashing/duplicateElement.java
@@ -0,0 +1,26 @@
+package Hashing;
+
+import java.util.HashSet;
+
+public class duplicateElement {
+    public static boolean checkDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }else{
+                set.add(nums[i]);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums[] ={1,2,3,1};
+
+        System.out.println("the array contains duplicate number -> "+ checkDuplicate(nums));
+    }
+}
