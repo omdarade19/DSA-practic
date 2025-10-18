b30595da113d1a2f4c315983d4fd1e1191f57392
omdarade19 <omdarade1944@gmail.com>
2025-10-18 11:30:14 +0530

    remove Duplicate

---
+++ Arrays/removeDuplicate.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class removeDuplicate {
+
+    public static int remove(int nums[]){
+
+        int k = 1;
+        for(int i=1; i< nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int nums[] ={0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println("the k value is -> "+remove(nums));
+
+        for(int num : nums){
+            System.out.print(num+"  ");
+        }
+    }
+}
