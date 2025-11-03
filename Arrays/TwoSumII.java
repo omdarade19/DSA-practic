34c2c796953fe5993e70bea96f124204bdb05444
omdarade19 <omdarade1944@gmail.com>
2025-11-03 06:41:19 +0530

    Two Sum II

---
+++ Arrays/TwoSumII.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+public class TwoSumII {
+    //Leetcode 167
+
+    //Given a 1-indexed array of integers numbers that is already sorted in
+    // non-decreasing order, find two numbers such that they add up to a specific
+    // target number. Let these two numbers be numbers[index1] and numbers[index2]
+    // where 1 <= index1 < index2 <= numbers.length.
+    //
+    //Return the indices of the two numbers, index1 and index2, added by one as an
+    // integer array [index1, index2] of length 2.
+    public static int[] twoSum(int[] numbers, int target) {
+
+        int i=0,
+                j=numbers.length-1;
+
+        while(i <= j){
+            if(numbers[i]+numbers[j] == target){
+                return new int[]{i+1, j+1};
+            }else if(numbers[i]+numbers[j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int nums[] ={2,7,11,15};
+        int target = 9;
+        int ans[] =twoSum(nums,target);
+
+        for(int num:ans){
+            System.out.print(num+" ");
+        }
+    }
+}
