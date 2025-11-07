e2a3e0f4504325f14c6113b5801eec6085943282
omdarade19 <omdarade1944@gmail.com>
2025-11-07 06:38:15 +0530

    reverse integer

---
+++ String/reverseInt.java
@@ -0,0 +1,46 @@
+package String;
+
+public class reverseInt {
+    //Leetcode 7
+
+    //Given a signed 32-bit integer x, return x with its digits reversed.
+    // If reversing x causes the value to go outside the signed 32-bit integer range
+    // [-231, 231 - 1], then return 0.
+    //
+    //Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+    public static int reverse(int x) {
+
+        int rev =0;
+
+        while(x != 0){
+            int dig = x%10;
+
+            if(rev >Integer.MAX_VALUE/10){
+                return 0;
+            }else if(rev < Integer.MIN_VALUE/10  ){
+                return 0;
+            }else if(rev == Integer.MAX_VALUE/10 && dig >7){
+                return 0;
+            }else if(rev == Integer.MIN_VALUE/10 && dig < -8){
+                return 0;
+            }
+
+            rev = rev*10 + dig;
+
+            x = x/10;
+
+
+        }
+
+
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        int x = -123;
+
+        System.out.println("the number is ->"+x);
+        System.out.println("the rev number is -> "+reverse(x));
+    }
+}
--- String/reverseStringArray.java
+++ String/reverseStringArray.java
@@ -1,6 +1,12 @@
 package String;
 
 public class reverseStringArray {
+    //Leetcode 344
+
+    //Write a function that reverses a string. The input string is given
+    // as an array of characters s.
+    //
+    //You must do this by modifying the input array in-place with O(1) extra memory.
     public static void reverse(char s[]){
         int lp=0, rp=s.length-1;
 
