a7c8b35b6122c8dfe26df1671a08b732fd666bf7
omdarade19 <omdarade1944@gmail.com>
2025-11-12 06:33:27 +0530

    number plaindrom

---
+++ Patterns/twoPointers/pakindromNumber.java
@@ -0,0 +1,27 @@
+package Patterns.twoPointers;
+
+public class pakindromNumber {
+
+    public static boolean isPalindrome(int x) {
+        if(x <0){
+            return false;
+        }
+
+        int copy = x;
+        int num =0;
+        while(x !=0){
+            int digit = x%10;
+            x = x/10;
+
+            num = num *10 + digit;
+        }
+
+        return num == copy;
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+
+        System.out.print("is this number is palindrom -> "+isPalindrome(x));
+    }
+}
