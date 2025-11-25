3730308061b6c13a3c511274461e1271b70a672a
omdarade19 <omdarade1944@gmail.com>
2025-11-25 13:50:32 +0530

    add Binary

---
+++ addBinary.java
@@ -0,0 +1,30 @@
+public class addBinary {
+
+    public static String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        StringBuilder ans = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            ans.append(sum % 2);   // 0 or 1
+            carry = sum / 2;       // new carry
+        }
+
+        return ans.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String a ="11";
+        String b ="1";
+
+        System.out.print("the addition of two binary string is -> "+ addBinary(a,b));
+    }
+}
