1e9481ecf713471afc494b35fe4f9b957a0adb6e
omdarade19 <omdarade1944@gmail.com>
2025-11-27 22:24:10 +0530

    Roman to Int

---
+++ String/romanToInt.java
@@ -0,0 +1,42 @@
+package String;
+
+public class romanToInt {
+        public static int romanToInt(String s) {
+            int result = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                int curr = value(s.charAt(i));
+
+                // If next value exists and is greater → subtract
+                if (i + 1 < s.length() && curr < value(s.charAt(i + 1))) {
+                    result -= curr;
+                } else {
+                    result += curr;
+                }
+            }
+
+            return result;
+        }
+
+        private static int value(char c) {
+            switch (c) {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+
+    public static void main(String[] args) {
+
+            String s = "MCMXCIV";
+
+            System.out.print("the Roman to Integer is -> "+romanToInt(s));
+    }
+    }
+
+
